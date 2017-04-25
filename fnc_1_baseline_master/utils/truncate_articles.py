c91fbef2e2aa5fec47ae9e0052c2b45ac0b4c140
Aishwarya Nirmal <anirmal@attu4.cs.washington.edu>
2017-04-24 21:54:22 -0700

    SVM YAY

--- fnc_1_baseline_master/utils/truncate_articles.py
+++ fnc_1_baseline_master/utils/truncate_articles.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def trunc_articles():
-	df = pd.read_csv("/homes/iws/liliang/WindowsFolders/NLPCapstone/fnc_1_baseline_master/fnc-1/train_bodies.csv")
+	df = pd.read_csv("fnc_1_baseline_master/fnc-1/train_bodies.csv")
 	articles = df["articleBody"]
 	id_article_dict = {}
 	for i, article in enumerate(articles):
